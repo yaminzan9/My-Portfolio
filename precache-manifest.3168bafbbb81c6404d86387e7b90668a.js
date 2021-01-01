98f9cf04e010f343b1f0d44ca8318193e756d044
yaminzan9 <zanoub9@outlook.com>
2021-01-01 20:42:02 +0800

    Updates

---
+++ precache-manifest.3168bafbbb81c6404d86387e7b90668a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30f0e068aad66f87d5e44ce2e1815fe2",
+    "url": "/My-Portfolio.io/index.html"
+  },
+  {
+    "revision": "d9711af2c045aa1259cc",
+    "url": "/My-Portfolio.io/static/css/main.38907027.chunk.css"
+  },
+  {
+    "revision": "1c0ea7a60795d7a68d0c",
+    "url": "/My-Portfolio.io/static/js/2.97b1cd2e.chunk.js"
+  },
+  {
+    "revision": "9fda8197ca6e25979aa843f42451bfaf",
+    "url": "/My-Portfolio.io/static/js/2.97b1cd2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9711af2c045aa1259cc",
+    "url": "/My-Portfolio.io/static/js/main.70664125.chunk.js"
+  },
+  {
+    "revision": "38033551e75c96820e6d",
+    "url": "/My-Portfolio.io/static/js/runtime-main.e4787d47.js"
+  },
+  {
+    "revision": "9c3b24f8b47028c48223321cd1158c40",
+    "url": "/My-Portfolio.io/static/media/myporty.9c3b24f8.jpg"
+  }
+]);
