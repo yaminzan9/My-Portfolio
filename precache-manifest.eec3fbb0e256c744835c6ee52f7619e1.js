b75623e511f5348b9e76edcbeb1304ee3705d6b9
yamin zann <68142796+yaminzan9@users.noreply.github.com>
2021-01-24 14:21:16 +0800

    Updates

---
+++ precache-manifest.eec3fbb0e256c744835c6ee52f7619e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6443207b8978ef2694ef5c8ef509c69",
+    "url": "/My-Portfolio.io/index.html"
+  },
+  {
+    "revision": "d154b2c919c90b1c6db7",
+    "url": "/My-Portfolio.io/static/css/main.23a12f50.chunk.css"
+  },
+  {
+    "revision": "b4ede121c133c317d761",
+    "url": "/My-Portfolio.io/static/js/2.a934a97f.chunk.js"
+  },
+  {
+    "revision": "9fda8197ca6e25979aa843f42451bfaf",
+    "url": "/My-Portfolio.io/static/js/2.a934a97f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d154b2c919c90b1c6db7",
+    "url": "/My-Portfolio.io/static/js/main.c3e1e877.chunk.js"
+  },
+  {
+    "revision": "38033551e75c96820e6d",
+    "url": "/My-Portfolio.io/static/js/runtime-main.e4787d47.js"
+  },
+  {
+    "revision": "9c3b24f8b47028c48223321cd1158c40",
+    "url": "/My-Portfolio.io/static/media/myporty.9c3b24f8.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/My-Portfolio.io/precache-manifest.462130275788af78ac5c824728a1980b.js"
+  "/My-Portfolio.io/precache-manifest.eec3fbb0e256c744835c6ee52f7619e1.js"
 );
 
 self.addEventListener('message', (event) => {
